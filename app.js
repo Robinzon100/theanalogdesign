043a30ab184a20d1e5a3336b0a00c23506ea75f7
Robinzon100 <robinzon.sokhashvilo@gmail.com>
2019-09-28 18:17:06 +0400

    asdasd

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const bodyParser = require("body-parser")
+const nodemon = require("nodemon")
+const path = require('path');
+
+// app.use(nodemon())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/", (req, res, next) =>{
+
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
